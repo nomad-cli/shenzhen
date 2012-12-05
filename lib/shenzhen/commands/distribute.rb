a4db506dd026cb66253ca5f7cb8c2c8fa1f040bc
Mattt Thompson <m@mattt.me>
2012-12-05 15:43:05 -0800

    Refactoring common distribute helper methods (which were being overridden anyway)
    Automatically detect matching .dsym.zip files on distribution

--- lib/shenzhen/commands/distribute.rb
+++ lib/shenzhen/commands/distribute.rb
@@ -1,1 +1,32 @@
 alias_command :distribute, :'distribute:testflight'
+
+private
+
+def determine_file!
+  files = Dir['*.ipa']
+  @file ||= case files.length
+            when 0 then nil
+            when 1 then files.first
+            else
+              @file = choose "Select an .ipa File:", *files
+            end
+end
+
+def determine_dsym!
+  dsym_files = Dir['*.dSYM.zip']
+  @dsym ||= case dsym_files.length
+            when 0 then nil
+            when 1 then dsym_files.first
+            else
+              dsym_files.detect do |dsym| 
+                File.basename(dsym, ".app.dSYM.zip") == File.basename(@file, ".ipa")
+              end or choose "Select a .dSYM.zip file:", *dsym_files
+            end
+end
+
+def determine_notes!
+  placeholder = %{What's new in this release: }
+  
+  @notes = ask_editor placeholder
+  @notes = nil if @notes == placeholder
+end
--- lib/shenzhen/plugins/testflight.rb
+++ lib/shenzhen/plugins/testflight.rb
@@ -58,10 +58,10 @@ command :'distribute:testflight' do |c|
     determine_dsym! unless @dsym = options.dsym
     say_error "Specified dSYM.zip file doesn't exist" if @dsym and !File.exist?(@dsym)
 
-    determine_api_token! unless @api_token = options.api_token
+    determine_testflight_api_token! unless @api_token = options.api_token
     say_error "Missing API Token" and abort unless @api_token
 
-    determine_team_token! unless @team_token = options.team_token
+    determine_testflight_team_token! unless @team_token = options.team_token
     
     determine_notes! unless @notes = options.notes
     say_error "Missing release notes" and abort unless @notes
@@ -86,38 +86,11 @@ command :'distribute:testflight' do |c|
 
   private
 
-  def determine_api_token!
+  def determine_testflight_api_token!
     @api_token ||= ask "API Token:"
   end
 
-  def determine_team_token!
+  def determine_testflight_team_token!
     @team_token ||= ask "Team Token:"
-  end
-
-  def determine_file!
-    files = Dir['*.ipa']
-    @file ||= case files.length
-              when 0 then nil
-              when 1 then files.first
-              else
-                @file = choose "Select an .ipa File:", *files
-              end
-  end
-
-  def determine_dsym!
-    dsym_files = Dir['*.dSYM.zip']
-    @dsym ||= case dsym_files.length
-              when 0 then nil
-              when 1 then dsym_files.first
-              else
-                @dsym = choose "Select a .dSYM.zip file:", *dsym_files
-              end
-  end
-
-  def determine_notes!
-    placeholder = %{What's new in this release: }
-    
-    @notes = ask_editor placeholder
-    @notes = nil if @notes == placeholder
   end
 end
