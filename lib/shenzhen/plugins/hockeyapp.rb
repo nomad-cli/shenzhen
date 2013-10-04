9f60ac55286806e4c4d95371d981f7a60119a3b4
Mattt Thompson <m@mattt.me>
2013-10-03 17:23:21 -0700

    [Issue #44] Fixing optional .ipa option for HockeyApp. Now to skip .ipa if present in current directory, one can specify a non-existent file.

--- lib/shenzhen/plugins/hockeyapp.rb
+++ lib/shenzhen/plugins/hockeyapp.rb
@@ -20,7 +20,7 @@ module Shenzhen::Plugins
       end
 
       def upload_build(ipa, options)
-        options[:ipa] = Faraday::UploadIO.new(ipa, 'application/octet-stream') unless ipa.nil?
+        options[:ipa] = Faraday::UploadIO.new(ipa, 'application/octet-stream') if ipa and File.exist?(ipa)
 
         if dsym_filename = options.delete(:dsym_filename)
           options[:dsym] = Faraday::UploadIO.new(dsym_filename, 'application/octet-stream')
