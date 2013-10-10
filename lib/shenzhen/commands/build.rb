8367dcc27e4de4a439ee0aa1c9faa89b732388c9
Mike Enriquez <mike@enriquez.me>
2013-10-10 13:27:25 -0400

    Pass -sdk option to xcodebuild. Fixes #66

--- lib/shenzhen/commands/build.rb
+++ lib/shenzhen/commands/build.rb
@@ -13,6 +13,7 @@ command :build do |c|
   c.option '--[no-]archive', 'Archive project after building'
   c.option '-d', '--destination DESTINATION', 'Destination. Defaults to current directory'
   c.option '-m', '--embed PROVISION', 'Sign .ipa file with .mobileprovision'
+  c.option '-sdk', '--sdk SDK', 'use SDK as the name or path of the base SDK when building the project'
 
   c.action do |args, options|
     validate_xcode_version!
@@ -23,6 +24,7 @@ command :build do |c|
     @xcodebuild_info = Shenzhen::XcodeBuild.info(:workspace => @workspace, :project => @project)
 
     @scheme = options.scheme
+    @sdk = options.sdk || 'iphoneos'
     @configuration = options.configuration
     @destination = options.destination || Dir.pwd
     FileUtils.mkdir_p(@destination) unless File.directory?(@destination)
@@ -42,7 +44,7 @@ command :build do |c|
     @configuration = options.configuration
     
     flags = []
-    flags << "-sdk iphoneos"
+    flags << "-sdk #{@sdk}"
     flags << "-workspace '#{@workspace}'" if @workspace
     flags << "-project '#{@project}'" if @project
     flags << "-scheme '#{@scheme}'" if @scheme
