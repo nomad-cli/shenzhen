074d3d57b49e2b26653d47b17875cc536bb6759e
Mattt Thompson <m@mattt.me>
2014-08-05 11:29:30 -0400

    Reordering require statements

--- lib/shenzhen/commands.rb
+++ lib/shenzhen/commands.rb
@@ -2,9 +2,9 @@ $:.push File.expand_path('../', __FILE__)
 
 require 'plugins/testflight'
 require 'plugins/hockeyapp'
+require 'plugins/deploygate'
 require 'plugins/ftp'
 require 'plugins/s3'
-require 'plugins/deploygate'
 
 require 'commands/build'
 require 'commands/distribute'
